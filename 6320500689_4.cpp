3f2cd3f79d3b591457fa4063fa61770071028dc1
b6320500689 <supitchaya.r@ku.th>
2021-03-04 16:59:21 +0700

    44444

--- 6320500689_4.cpp
+++ 6320500689_4.cpp
@@ -2,10 +2,10 @@
 #include <string.h>
 int main()
 {
-	  char s[1000],d[11][5]={"i", "of", "the","on", "at", "for", "with", "a", "an" , "in", "and"} ;
-  char m;
-  int i=0,j,k=0;
-  scanf("%[^\n]s",s);
-  printf ("%s",s);
-  return 0;
+	char s[1000],d[11][5]={"i", "of", "the","on", "at", "for", "with", "a", "an" , "in", "and"} ;
+  	char m;
+  	int i=0,j,k=0;
+  	scanf("%[^\n]s",s);
+  	printf ("%s",s);
+return 0;
 }
