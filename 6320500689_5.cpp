9e116941c3d1ffee82919c7d5269da6318cbc7e8
b6320500689 <supitchaya.r@ku.th>
2021-03-04 16:42:38 +0700

    5555

--- 6320500689_5.cpp
+++ 6320500689_5.cpp
@@ -4,94 +4,99 @@ int main()
 {
     int x,y,i=1,m;
     char a[50];
+    char b[50];
     scanf ("%d",&x);
     for (y=0; y<x; y++)
     {
         scanf ("%s",a);
         for (m=0; m<strlen(a); m++)
         {
-            if (a[m] == 'A')
-            {
-                if (i == 2)
-                {
-                    i = 3;
-                }
-                else if (i == 3)
-                {
-                    i = 2;
-                }
-            }
-            else if (a[m] == 'B')
-            {
-                if (i == 1)
-                {
-                    i = 4;
-                }
-                else if (i == 4)
-                {
-                    i = 1;
-                }
-            }
-            else if (a[m] == 'C')
-            {
-                if (i == 1)
-                {
-                    i = 3;
-                }
-                else if (i == 3)
-                {
-                    i = 1;
-                }
-                else if (i == 4)
-                {
-                    i = 2;
-                }
-                else if (i == 2)
-                {
-                    i = 4;
-                }
-            }
-            else if (a[m] == 'D')
-            {
-                if (i == 1)
-                {
-                    i = 2;
-                }
-                else if (i == 2)
-                {
-                    i = 1;
-                }
-                else if (i == 3)
-                {
-                    i = 4;
-                }
-                else if (i == 4)
-                {
-                    i = 3;
-                }
-            }
-            else if (a[m] == 'E')
-            {
-                if (i == 1)
-                {
-                    i = 4;
-                }
-                else if (i == 4)
-                {
-                    i = 1;
-                }
-                else if (i == 2)
-                {
-                    i = 3;
-                }
-                else if (i == 3)
-                {
-                    i = 2;
-                }
-            }
+        	scanf ("%s",b);
+        	for (m=0; m<strlen(a); m++)
+        	{
+            	if (a[m] == 'A')
+            	{
+                	if (i == 2)
+                	{
+                  		i = 3;
+                	}
+               		else if (i == 3)
+                	{
+                   		i = 2;
+                	}
+            	}
+            	else if (a[m] == 'B')
+            	{
+                	if (i == 1)
+                	{
+                    	i = 4;
+                	}
+                	else if (i == 4)
+                	{
+                    	i = 1;
+                	}
+            	}
+            	else if (a[m] == 'C')
+            	{
+                	if (i == 1)
+                	{
+                    	i = 3;
+                	}
+                	else if (i == 3)
+                	{
+                    	i = 1;
+                	}
+                	else if (i == 4)
+                	{
+                    	i = 2;
+                	}
+                	else if (i == 2)
+                	{
+                    	i = 4;
+                	}
+            	}
+            	else if (a[m] == 'D')
+            	{
+                	if (i == 1)
+                	{
+                    	i = 2;
+                	}
+                	else if (i == 2)
+                	{
+                    	i = 1;
+                	}
+                	else if (i == 3)
+                	{
+                    	i = 4;
+                	}
+                	else if (i == 4)
+                	{
+                    	i = 3;
+                	}
+            	}
+            	else if (a[m] == 'E')
+            	{
+                	if (i == 1)
+                	{
+                    	i = 4;
+                	}
+                	else if (i == 4)
+                	{
+                    	i = 1;
+                	}
+                	else if (i == 2)
+                	{
+                    	i = 3;
+                	}
+                	else if (i == 3)
+                	{
+                    	i = 2;
+                	}
+            	}
             printf("%d\n",i);
         	i = 1;
     	}
     return 0;
 	}
+}
 }
