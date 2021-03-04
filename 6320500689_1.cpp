b496af7d6a8cd169ee60562134b4af2e73cd1a9a
b6320500689 <supitchaya.r@ku.th>
2021-03-04 15:24:03 +0700

    11

--- 6320500689_1.cpp
+++ 6320500689_1.cpp
@@ -1,5 +1,15 @@
 #include <stdio.h>
 int main ()
 {
-	int
+	int x,y,a,b,c,m,n;
+	if (n>3 && n<1000)
+		scanf ("%d",&x);
+    for (n=0; n<x; x++)
+    {
+        scanf ("%d %d %d",&a,&b,&c);
+        n = (a*4)+(b*2)+c;
+        b[m] = n;
+    }
+    printf ("%d %d",);
+    return 0;
 }
