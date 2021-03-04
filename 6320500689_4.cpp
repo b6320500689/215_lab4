6e7229532e5162fc40704863f73cf3d39799a8e0
b6320500689 <supitchaya.r@ku.th>
2021-03-04 16:54:28 +0700

    4

---
+++ 6320500689_4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	  char s[1000],d[11][5]={"i", "of", "the","on", "at", "for", "with", "a", "an" , "in", "and"} ;
+  char m;
+  int i=0,j,k=0;
+  scanf("%[^\n]s",s);
+  return 0;
+}
